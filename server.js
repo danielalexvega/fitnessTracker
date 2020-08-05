75a027c9d321599f2a4a975c46b3496f3a64dfb3
Daniel Vega <danielalexvega@gmail.com>
2020-08-05 17:48:27 -0500

    working on routing

--- server.js
+++ server.js
@@ -3,3 +3,54 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+
+const Workout = require("./models/Workout.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+
+// GET ALL THE WORKOUTS
+app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+//CREATE A WORKOUT
+app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+//UPDATE A WORKOUT
+app.put("/api/workouts", ({ body }, res) => {
+    Workout.update(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
